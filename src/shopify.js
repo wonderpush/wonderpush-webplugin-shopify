df3999ae13db69337e22bbd17bc4c0c6924398e3
Stéphane JAIS <stephane@wonderpush.com>
2019-10-17 15:44:15 +0200

    Cart reminder implementation

--- src/shopify.js
+++ src/shopify.js
@@ -11,7 +11,7 @@
    * @property {Boolean} [disableCartReminder] - Set to true to disable cart reminder. Defaults to false.
    * @property {String} [cartReminderStrategy] - Sets the title of the cart reminder push notification to the latest, least or most expensive product in the cart. Valid values are 'latest', 'most-expensive' and 'least-expensive'. Defaults to 'latest'
    * @property {String} [cartReminderMessage] - Sets the message of the cart reminder push notification. Defaults to “Order before it's too late!”.
-   * @property {String} [cartReminderDestination] - Determines where the user is taken upon cart reminder push notification click. Valid values are 'homepage', 'cart', 'checked'. Defaults to 'cart'.
+   * @property {String} [cartReminderDestination] - Determines where the user is taken upon cart reminder push notification click. Valid values are 'homepage', 'cart', 'checkout'. Defaults to 'cart'.
    * @property {String} [cartReminderDisableImage] - When true, the product image is not added to the notification. Defaults to false.
    */
   /**
@@ -21,6 +21,7 @@
    */
   WonderPush.registerPlugin("shopify", {
     window: function OptinBell(WonderPushSDK, options) {
+      window.WonderPush = window.WonderPush || [];
       var translations = {
         "fr": {
           "Order before it's too late!": "Commandez avant qu'il ne soit trop tard !",
@@ -48,8 +49,119 @@
         if (translations.hasOwnProperty(language) && translations[language][text]) return translations[language][text];
         return text;
       };
+      var defaultMessage = _('Order before it\'s too late!');
 
-      console.log('[wonderpush-shopify] ready', options);
+      var cartClient = new function() {
+        this.get = function() {
+          return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/cart.js');
+            xhr.onload = function() {
+              if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(JSON.parse(xhr.responseText));
+                return;
+              }
+              reject(new Error('Invalid response'));
+            };
+            xhr.onerror = function(error) {
+              reject(error);
+            };
+            xhr.send();
+          });
+        };
+      }();
+
+      var cartReminder = new function(frequency) {
+        if (!frequency) frequency = 3000;
+        var timeoutId;
+        var running = false;
+
+        this.start = function() {
+          var run = function() {
+            if (!running) return;
+            this.update();
+            setTimeout(run, frequency);
+          }.bind(this);
+
+          running = true;
+          if (timeoutId) return;
+          run();
+        }.bind(this);
+
+        this.stop = function() {
+          running = false;
+          if (timeoutId) {
+            clearTimeout(timeoutId);
+            timeoutId = null;
+          }
+        }.bind(this);
+
+        var propertiesFromCart = function(cart) {
+          var product = cart && cart.items && cart.items.length ? cart.items[0] : undefined;
+
+          var products = cart && cart.items || [];
+          if (!products.length) {
+            return {
+              string_cartReminderProductName: null,
+              string_cartReminderMessage: null,
+              string_cartReminderUrl: null,
+              string_cartReminderPictureUrl: null,
+            }
+          }
+          var product;
+          switch(options.cartReminderStrategy || 'latest') {
+            case 'most-expensive':
+              products.forEach(function(p) {
+                if (!product) product = p;
+                else if (p.final_line_price >= product.final_line_price) product = p;
+              });
+              break;
+            case 'least-expensive':
+              products.forEach(function(p) {
+                if (!product) product = p;
+                else if (p.final_line_price <= product.final_line_price) product = p;
+              });
+              break;
+            case 'latest':
+            default:
+              product = products[0]; // Yes, latest is the first item in shopify's list.
+              break;
+          }
+
+          var url;
+          switch(options.cartReminderDestination || 'cart') {
+            case 'product':
+              url = new URL(product.url, window.location);
+              break;
+            case 'cart':
+              url = new URL('/cart', window.location);
+              break;
+            case 'homepage':
+              url = new URL('/', window.location);
+              break;
+            case 'checkout':
+              url = new URL('/checkout', window.location);
+              break;
+          }
+          return {
+            string_cartReminderProductName: product.product_title || null,
+            string_cartReminderMessage: options && options.cartReminderMessage || defaultMessage,
+            string_cartReminderUrl: url.href,
+            string_cartReminderPictureUrl: options.cartReminderDisableImage ? null : product.image || null,
+          }
+        };
+        this.update = function() {
+          cartClient.get()
+            .then(function(cart) {
+              window.WonderPush.push(function() {
+                var properties = propertiesFromCart(cart);
+                window.WonderPush.putProperties(properties);
+              });
+            });
+        }.bind(this);
+      }();
+
+      if (!options.disableCartReminder) cartReminder.start();
     }
   });
 })();
